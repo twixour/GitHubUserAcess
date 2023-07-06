3ef338edaf5ee2822e3ac17cc1ba839ef54e3620
twixour <twixour@gmail.com>
2023-07-06 12:59:56 +0530

    Initial Commit

---
+++ GitHubUserAcess/GitHubUserAcessApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUserAcessApp.swift
+//  GitHubUserAcess
+//
+//  Created by Raushan Kashyap on 06/07/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUserAcessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
